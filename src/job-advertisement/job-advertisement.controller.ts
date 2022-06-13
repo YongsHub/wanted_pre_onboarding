76196cf9ab90462200c6254e02d5df9294553e89
김태용 <amico741@naver.com>
2022-06-13 17:29:03 +0900

    ✨#3
    채용공고 수정

--- src/job-advertisement/job-advertisement.controller.ts
+++ src/job-advertisement/job-advertisement.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Param, ParseIntPipe, Patch, Post, Query } from '@nestjs/common';
+import { Board } from 'src/entities/board.entity';
 import { CreateBoardDto } from './dto/board.dto';
 import { JobAdvertisementService } from './job-advertisement.service';
 
@@ -8,11 +9,17 @@ export class JobAdvertisementController {
     
     @Patch('/advertisement/:id') //회사에 채용공고 등록
     createAdvertisement(
-        @Param('id', ParseIntPipe) id : number, // 회사 id
-        @Body() createBoardDto : CreateBoardDto // 채용공고 DTO
+        @Param('id', ParseIntPipe) id: number, // 회사 id
+        @Body() createBoardDto: CreateBoardDto // 채용공고 DTO
     ): Promise<{company_id:number, position: string, reward: string, description: string, stack: string}> {
         return this.postingService.create(id, createBoardDto);
     }
 
-
+    @Patch('/:id') // 회사 id를 제외한 내용 수정
+    updateAdvertisement(
+        @Param('id', ParseIntPipe) id: number,
+        @Body() createBoardDto: CreateBoardDto,
+    ): Promise<Board>{
+        return this.postingService.update(id, createBoardDto);
+    }
 }
